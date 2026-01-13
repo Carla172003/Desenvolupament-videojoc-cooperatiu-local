f0d530c4da1e514a9840f4bc6268e761c5feb66c
Carla172003 <carla.lopez.campos@estudiantat.upc.edu>
2026-01-13 19:01:52 +0100

    Millorant estructura sistema puntuacio

--- Assets/Scripts/ControladorPuntuacio.cs
+++ Assets/Scripts/ControladorPuntuacio.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 /// <summary>
 /// Controlador de la puntuació del joc.
+/// Utilitza el patró Strategy per al càlcul d'estrelles.
 /// </summary>
 public class ControladorPuntuacio : MonoBehaviour
 {
@@ -17,11 +18,20 @@ public class ControladorPuntuacio : MonoBehaviour
     [SerializeField] private int puntuacioMin2Estrelles = 9600;
     [SerializeField] private int puntuacioMin3Estrelles = 12600;
     
+    // Estratègia per calcular estrelles (patró Strategy)
+    private ISistemaPuntuacio estrategiaEstrelles;
+    
     public int numEstrelles { get; private set; } = 0;
 
     void Awake()
     {
         Instance = this;
+        
+        // Inicialitzar estratègia de càlcul d'estrelles
+        estrategiaEstrelles = new Puntuacio3Estrelles(
+            puntuacioMin2Estrelles, 
+            puntuacioMin3Estrelles
+        );
     }
 
     void Start()
@@ -62,36 +72,23 @@ public class ControladorPuntuacio : MonoBehaviour
     }
     
     /// <summary>
-    /// Calcula el nombre d'estrelles obtingudes segons la puntuació final.
-    /// Si partida no completada: 0 estrelles.
-    /// Si completada: compara amb els llindars definits.
+    /// Calcula el nombre d'estrelles obtingudes utilitzant l'estratègia de càlcul (patró Strategy).
     /// </summary>
     /// <param name="puntuacioFinal">Puntuació final de la partida.</param>
     /// <param name="partidaCompletada">True si la partida s'ha completat amb èxit.</param>
     /// <returns>Nombre d'estrelles (0-3).</returns>
     public int CalcularEstrelles(int puntuacioFinal, bool partidaCompletada)
     {
-        // 0 estrelles si la partida no s'ha completat
-        if (!partidaCompletada)
-        {
-            numEstrelles = 0;
-            return numEstrelles;
-        }
-
-        // Determinar estrelles segons els llindars configurats
-        if (puntuacioFinal >= puntuacioMin3Estrelles)
-        {
-            numEstrelles = 3;
-        }
-        else if (puntuacioFinal >= puntuacioMin2Estrelles)
-        {
-            numEstrelles = 2;
-        }
-        else
-        {
-            numEstrelles = 1;
-        }
-        
+        numEstrelles = estrategiaEstrelles.CalcularEstrelles(puntuacioFinal, partidaCompletada);
         return numEstrelles;
+    }
+    
+    /// <summary>
+    /// Canvia l'estratègia de càlcul d'estrelles.
+    /// </summary>
+    /// <param name="novaEstrategia">Nova estratègia a utilitzar.</param>
+    public void CanviarEstrategiaEstrelles(ISistemaPuntuacio novaEstrategia)
+    {
+        estrategiaEstrelles = novaEstrategia;
     }
 }
